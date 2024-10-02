858febb896375e6e784e5b3d92c587d8f2ed76d0
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-10-02 10:59:53 +0000

    volumes addition

---
+++ start.sh
@@ -0,0 +1,10 @@
+# docker compose build
+# docker compose up
+# docker exec -it <container name> /bin/bash
+# psql -U myuser -d mydatabase -p mypassword
+# use <database name>
+# select * from table;
+# docker restart <container name>
+# docker --force delete <container name>
+# docker ps
+# docker volume ls
